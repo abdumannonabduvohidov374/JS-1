a5870c594114aec32b3b8eeed512d5800483d086
Abdumannon <111962763+abdumannonabduvohidov374@users.noreply.github.com>
2022-10-24 20:43:31 +0300

    Add files via upload

---
+++ js/script.js
@@ -0,0 +1,81 @@
+// Settings
+
+"use strict";
+
+// Alert
+
+alert ('Abdumannon Haqida Ruxsat Berasizmi!')
+
+// Uyga Vazifa || Header && 1!
+
+const object = {
+        Doniyorbek: {
+            Ismi: 'Doniyorbek',
+            Familiyasi: 'Rasulov',
+            Yoshi: 38,
+            Tel_Raqami: '+998 88 999 00 37'
+        },
+        Minuraxon: {
+            Ismi: 'Minuraxon',
+            Familiyasi: 'Rasulova',
+            Yoshi: 36,
+            Tel_Raqami: '+998 94 383 84 86'
+        },
+        Abdumannon: {
+            Ismi: 'Abdumannon',
+            Familiyasi: 'Abduvohidov',
+            Yoshi: 13,
+            Tel_Raqami: '+998 93 993 45 48'
+        }
+}
+
+// --------------------------------------------------------
+
+console.log('1------------------------------------------------------------------------------------------------1');
+
+// Console Body
+
+console.log('Otasining ismi: ' + object.Doniyorbek.Ismi + ", ",
+            'Familiyasi: ' + object.Doniyorbek.Familiyasi + ", ",
+            'Yoshi: ' + object.Doniyorbek.Yoshi + ", ",
+            'Telefon Raqami: ' + object.Doniyorbek.Tel_Raqami + ". ",);
+console.log('Onasining ismi: ' + object.Minuraxon.Ismi + ", ",
+            'Familiyasi: ' + object.Minuraxon.Familiyasi + ", ",
+            'Yoshi: ' + object.Minuraxon.Yoshi + ", ",
+            'Telefon Raqami: ' + object.Minuraxon.Tel_Raqami + ". ",);
+console.log('Ozini ismi: ' + object.Abdumannon.Ismi + ", ",
+            'Familiyasi: ' + object.Abdumannon.Familiyasi + ", ",
+            'Yoshi: ' + object.Abdumannon.Yoshi + ", ",
+            'Telefon Raqami: ' + object.Abdumannon.Tel_Raqami + ". ",);
+
+// -----------------------------------------------------------
+
+console.log('2------------------------------------------------------------------------------------------------2');
+
+// Console Body 2
+
+console.log(object);
+
+// ------------------------------------------------------------
+
+console.log('3------------------------------------------------------------------------------------------------3');
+
+// 1
+
+console.log(object.Doniyorbek);
+
+// 2
+
+console.log(object.Minuraxon);
+
+// 3
+
+console.log(object.Abdumannon);
+
+// Tugadi
+
+let footer = 'Tugadi';
+
+// Console Footer
+
+console.log(footer + '.');
